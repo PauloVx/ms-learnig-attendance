da5ab4748d9773706462d9cdb3ab24b25f57dee0
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-18 19:10:49 -0300

    Fixed wrong injection of repository

--- src/main/java/com/sysmap/mslearnigattendance/controllers/AttendanceController.java
+++ src/main/java/com/sysmap/mslearnigattendance/controllers/AttendanceController.java
@@ -1,16 +1,48 @@
 package com.sysmap.mslearnigattendance.controllers;
 
-import com.sysmap.mslearnigattendance.repositories.AttendanceRepository;
+import com.sysmap.mslearnigattendance.controllers.models.RegisterAttendanceInput;
+import com.sysmap.mslearnigattendance.entities.Attendance;
+import com.sysmap.mslearnigattendance.services.AttendanceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Controller
+@RequestMapping("/v1")
 public class AttendanceController {
 
-    private AttendanceRepository attendanceRepository;
+    private AttendanceService attendanceService;
 
     public AttendanceController(
-        AttendanceRepository attendanceRepository
+        AttendanceService attendanceService
     ) {
-        this.attendanceRepository = attendanceRepository;
+        this.attendanceService = attendanceService;
+    }
+
+    @PostMapping("/course/{courseId}/student/{studentId}/attendance")
+    public ResponseEntity<Void> registerNewAttendance(
+        @PathVariable UUID courseId,
+        @PathVariable UUID studentId,
+        @RequestBody RegisterAttendanceInput requestBody
+    ) {
+        var attendance = new Attendance();
+        attendance.setAttendanceId(UUID.randomUUID());
+        attendance.setStudentId(studentId);
+        attendance.setCourseId(courseId);
+        attendance.setClassDate(LocalDateTime.now());
+        attendance.setAttendanceStatus(requestBody.getAttendanceStatus());
+
+        var savedWithSuccess = this.attendanceService.save(attendance);
+
+        if(!savedWithSuccess) return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
