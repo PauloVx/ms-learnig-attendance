3d4cbac9a98ae18ce4f03058e30df3d2e1fbd1bc
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-18 18:34:39 -0300

    Create AttendanceRepository.java

---
+++ src/main/java/com/sysmap/mslearnigattendance/repositories/AttendanceRepository.java
@@ -0,0 +1,11 @@
+package com.sysmap.mslearnigattendance.repositories;
+
+import com.sysmap.mslearnigattendance.entities.Attendance;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AttendanceRepository extends CrudRepository<Attendance, UUID> {
+}
