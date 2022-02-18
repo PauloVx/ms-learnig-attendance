18cc53ffe691609f6d918ff55299c739c2db252c
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-18 12:23:36 -0300

    Created Project

---
+++ src/main/java/com/sysmap/mslearnigattendance/MsLearnigAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.sysmap.mslearnigattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLearnigAttendanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLearnigAttendanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysmap/mslearnigattendance/MsLearnigAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmap.mslearnigattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLearnigAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
