c43d36a0bfb8a03681a0ea7ac46295e5d1b8c56f
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-18 19:37:32 -0300

    Validating courseId

--- src/main/java/com/sysmap/mslearnigattendance/controllers/AttendanceController.java
+++ src/main/java/com/sysmap/mslearnigattendance/controllers/AttendanceController.java
@@ -3,6 +3,7 @@ package com.sysmap.mslearnigattendance.controllers;
 import com.sysmap.mslearnigattendance.controllers.models.RegisterAttendanceInput;
 import com.sysmap.mslearnigattendance.entities.Attendance;
 import com.sysmap.mslearnigattendance.services.AttendanceService;
+import com.sysmap.mslearnigattendance.services.CourseAPIService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,11 +20,14 @@ import java.util.UUID;
 public class AttendanceController {
 
     private AttendanceService attendanceService;
+    private CourseAPIService courseAPIService;
 
     public AttendanceController(
-        AttendanceService attendanceService
+        AttendanceService attendanceService,
+        CourseAPIService courseAPIService
     ) {
         this.attendanceService = attendanceService;
+        this.courseAPIService = courseAPIService;
     }
 
     @PostMapping("/course/{courseId}/student/{studentId}/attendance")
@@ -40,8 +44,9 @@ public class AttendanceController {
         attendance.setAttendanceStatus(requestBody.getAttendanceStatus());
 
         var savedWithSuccess = this.attendanceService.save(attendance);
+        var isCourseIdValid = this.courseAPIService.isCourseIdValid(attendance.getCourseId());
 
-        if(!savedWithSuccess) return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        if(!savedWithSuccess || !isCourseIdValid) return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
