5b8365f09d4bc6df92a484cac58df510875bd111
Paulo Vitor Soleiro Rodrigues <paulovt1999@gmail.com>
2022-02-18 21:27:58 -0300

    Added indexed annotation to allow redis to find attendances by studentId

--- src/main/java/com/sysmap/mslearnigattendance/entities/Attendance.java
+++ src/main/java/com/sysmap/mslearnigattendance/entities/Attendance.java
@@ -2,19 +2,24 @@ package com.sysmap.mslearnigattendance.entities;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @RedisHash("Attendance")
+@TypeAlias("Attendance")
 public class Attendance
 {
     @Id
     private UUID attendanceId;
 
+    @Indexed()
     private UUID studentId;
+
     private UUID courseId;
     private LocalDateTime classDate;
     private Boolean attendanceStatus;
